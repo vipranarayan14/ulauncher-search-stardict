76aba6a857cc02ae2d290368184d3f377d243725
Prasanna Venkatesh T S <vipranarayan14@gmail.com>
2021-01-30 23:29:12 +0530

    fix missing return & uncommented code

--- search.py
+++ search.py
@@ -11,15 +11,15 @@ def initDictionary(dict_path):
 
 def search(dictionary, query):
     try:
-        dictionary.dict[query]
+        return dictionary.dict[query]
     except KeyError:
         raise WordNotInDictionary(query)
 
 
-_dict = initDictionary(
-    dict_path="/home/prasanna/App Files/data/dictdata/shabda-sAgara/shabda-sAgara"
-)
-
-result = search(_dict, "fdsf")
-
-print(result)
+# _dict = initDictionary(
+#     dict_path="/home/prasanna/App Files/data/dictdata/shabda-sAgara/shabda-sAgara"
+# )
+#
+# result = search(_dict, "fdsf")
+#
+# print(result)
